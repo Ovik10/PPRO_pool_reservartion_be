032f1c0522dbe02bf83755b5aa6b795829a293fa
Jindra <svobodajindrich@centrum.cz>
2022-02-03 10:03:23 +0100

    fix

--- src/main/java/cz/ppro/poolapp/be/api/RoleController.java
+++ src/main/java/cz/ppro/poolapp/be/api/RoleController.java
@@ -1,16 +1,11 @@
 package cz.ppro.poolapp.be.api;
 
-import cz.ppro.poolapp.be.exception.ResourceNotFoundException;
 import cz.ppro.poolapp.be.model.Role;
 import cz.ppro.poolapp.be.repository.AccountRepository;
 import cz.ppro.poolapp.be.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3006")
--- src/main/java/cz/ppro/poolapp/be/api/TicketTypeController.java
+++ src/main/java/cz/ppro/poolapp/be/api/TicketTypeController.java
@@ -1,15 +1,9 @@
 package cz.ppro.poolapp.be.api;
 
-import cz.ppro.poolapp.be.exception.ResourceNotFoundException;
-import cz.ppro.poolapp.be.model.Role;
-import cz.ppro.poolapp.be.model.Ticket;
 import cz.ppro.poolapp.be.model.TicketType;
 import cz.ppro.poolapp.be.repository.TicketTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3006")
